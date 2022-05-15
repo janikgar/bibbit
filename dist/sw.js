1af3fe100e77fd6c14a97ea4b0c7c8a753a63a19
J. Garcia <janikgar@gmail.com>
2022-05-15 13:08:38 -0700

    log cache pop isues

--- dist/sw.js
+++ dist/sw.js
@@ -9,7 +9,9 @@ const initCache = async () => {
     "/main.js",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.js",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.css",
-  ])
+  ]).catch((err) => {
+    console.log(`error populating cache: ${err}`);
+  })
 }
 
 self.addEventListener("install", (event) => {
